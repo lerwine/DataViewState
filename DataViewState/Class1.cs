7dd3a2df8c314fa331245768b17078a33a8c5562
lerwine <lenny@erwinefamily.net>
2014-01-23 07:06:29 -0500

    Initial Check-in

---
+++ DataViewState/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataViewState
+{
+    public class Class1
+    {
+        public Class1()
+        {
+            using (DataViewStateEntities entities = new DataViewStateEntities())
+            {
+                System.Data.Objects.ObjectResult<GetElementValues_Result> r = entities.GetElementValues(Guid.Empty, "", "", "nvarchar(max)");
+
+            }
+        }
+    }
+}
